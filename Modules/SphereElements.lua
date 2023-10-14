c9796fdd683a9d10511e30384432779b8b4393cf
AlexanderKlyuchnikov <AlexanderK1707@yandex.ru>
2023-10-15 02:42:02 +0300

    Add new colours to setTextures

--- Modules/SphereElements.lua
+++ Modules/SphereElements.lua
@@ -81,18 +81,82 @@ end
 
 local function setTextures(noteskin)
 	noteskin:setTextures({
-		{greyshortnote = "Notes/GreyShortNote.png"},
-		{greylongnote = "Notes/GreyLongNote.png"},
-		{greylongnotebody = "Notes/GreyLongNoteBody.png"},
+		{whiteshortnote = "Notes/WhiteShortNote.png"},
+		{whitelongnote = "Notes/WhiteLongNote.png"},
+		{whitelongnotebody = "Notes/WhiteLongNoteBody.png"},
+		{longwhiteshortnote = "Notes/LongWhiteShortNote.png"},
+		{longwhitelongnote = "Notes/LongWhiteLongNote.png"},
+		{longwhitelongnotebody = "Notes/LongWhiteLongNoteBody.png"},
+
 		{blueshortnote = "Notes/BlueShortNote.png"},
 		{bluelongnote = "Notes/BlueLongNote.png"},
 		{bluelongnotebody = "Notes/BlueLongNoteBody.png"},
-		{pinkshortnote = "Notes/PinkShortNote.png"},
-		{pinklongnote = "Notes/PinkLongNote.png"},
-		{pinklongnotebody = "Notes/PinkLongNoteBody.png"},
+		{longblueshortnote = "Notes/LongBlueShortNote.png"},
+		{longbluelongnote = "Notes/LongBlueLongNote.png"},
+		{longbluelongnotebody = "Notes/LongBlueLongNoteBody.png"},
+
+		{yellowshortnote = "Notes/YellowShortNote.png"},
+		{yellowlongnote = "Notes/YellowLongNote.png"},
+		{yellowlongnotebody = "Notes/YellowLongNoteBody.png"},
+		{longyellowshortnote = "Notes/LongYellowShortNote.png"},
+		{longyellowlongnote = "Notes/LongYellowLongNote.png"},
+		{longyellowlongnotebody = "Notes/LongYellowLongNoteBody.png"},
+
+		{redshortnote = "Notes/RedShortNote.png"},
+		{redlongnote = "Notes/RedLongNote.png"},
+		{redlongnotebody = "Notes/RedLongNoteBody.png"},
 		{longredshortnote = "Notes/LongRedShortNote.png"},
 		{longredlongnote = "Notes/LongRedLongNote.png"},
 		{longredlongnotebody = "Notes/LongRedLongNoteBody.png"},
+
+		{violetshortnote = "Notes/VioletShortNote.png"},
+		{violetlongnote = "Notes/VioletLongNote.png"},
+		{violetlongnotebody = "Notes/VioletLongNoteBody.png"},
+		{longvioletshortnote = "Notes/LongVioletShortNote.png"},
+		{longvioletlongnote = "Notes/LongVioletLongNote.png"},
+		{longvioletlongnotebody = "Notes/LongVioletLongNoteBody.png"},
+
+		{pinkshortnote = "Notes/PinkShortNote.png"},
+		{pinklongnote = "Notes/PinkLongNote.png"},
+		{pinklongnotebody = "Notes/PinkLongNoteBody.png"},
+		{longpinkshortnote = "Notes/LongPinkShortNote.png"},
+		{longpinklongnote = "Notes/LongPinkLongNote.png"},
+		{longpinklongnotebody = "Notes/LongPinkLongNoteBody.png"},
+
+		{orangeshortnote = "Notes/OrangeShortNote.png"},
+		{orangelongnote = "Notes/OrangeLongNote.png"},
+		{orangelongnotebody = "Notes/OrangeLongNoteBody.png"},
+		{longorangeshortnote = "Notes/LongOrangeShortNote.png"},
+		{longorangelongnote = "Notes/LongOrangeLongNote.png"},
+		{longorangelongnotebody = "Notes/LongOrangeLongNoteBody.png"},
+
+		{cyanshortnote = "Notes/CyanShortNote.png"},
+		{cyanlongnote = "Notes/CyanLongNote.png"},
+		{cyanlongnotebody = "Notes/CyanLongNoteBody.png"},
+		{longcyanshortnote = "Notes/LongCyanShortNote.png"},
+		{longcyanlongnote = "Notes/LongCyanLongNote.png"},
+		{longcyanlongnotebody = "Notes/LongCyanLongNoteBody.png"},
+
+		{greenshortnote = "Notes/GreenShortNote.png"},
+		{greenlongnote = "Notes/GreenLongNote.png"},
+		{greenlongnotebody = "Notes/GreenLongNoteBody.png"},
+		{longgreenshortnote = "Notes/LongGreenShortNote.png"},
+		{longgreenlongnote = "Notes/LongGreenLongNote.png"},
+		{longgreenlongnotebody = "Notes/LongGreenLongNoteBody.png"},
+
+		{greyshortnote = "Notes/GreyShortNote.png"},
+		{greylongnote = "Notes/GreyLongNote.png"},
+		{greylongnotebody = "Notes/GreyLongNoteBody.png"},
+		{longgreyshortnote = "Notes/LongGreyShortNote.png"},
+		{longgreylongnote = "Notes/LongGreyLongNote.png"},
+		{longgreylongnotebody = "Notes/LongGreyLongNoteBody.png"},
+
+		{blackshortnote = "Notes/BlackShortNote.png"},
+		{blacklongnote = "Notes/BlackLongNote.png"},
+		{blacklongnotebody = "Notes/BlackLongNoteBody.png"},
+		{longblackshortnote = "Notes/LongBlackShortNote.png"},
+		{longblacklongnote = "Notes/LongBlackLongNote.png"},
+		{longblacklongnotebody = "Notes/LongBlackLongNoteBody.png"},
 	})
 end
 
